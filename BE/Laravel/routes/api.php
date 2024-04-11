04e9b6cc330f444ffb5bb7f67b5fb9c4e69f2562
ntq2k3work <ntq2k3.work@gmail.com>
2024-04-11 10:16:26 +0700

    edit login,authenticated by JWT

--- BE/Laravel/routes/api.php
+++ BE/Laravel/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\ForgotController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\SearchController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,12 +19,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return '123';
-// });
-Route::post('/register',[RegisterController::class,'register']) -> name('register');
-Route::post('/login',[LoginController::class,'login']) -> name('login');
-Route::post('/forgot',[ForgotController::class,'forgot']) -> name('forgot');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return auth() -> user();
+});
 Route::get('/search/{keyword?}',[SearchController::class,'search']) -> name('search');
+Route::post('/register',[RegisterController::class,'register']) -> name('register');
+Route::post('/forgot',[ForgotController::class,'forgot']) -> name('forgot');
 
+Route::group([
 
+    'middleware' => 'api',
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class,'login']) -> name('login');
+    Route::post('logout', [AuthController::class,'logout']) -> name('logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
