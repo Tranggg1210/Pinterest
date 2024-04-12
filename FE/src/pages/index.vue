a4afa38c350c9e73013ad94a8d426cd8857adaaf
haniekid <thuharosy2003@gmail.com>
2024-04-12 23:54:22 +0700

    :seedling: add home page interface

--- FE/src/pages/index.vue
+++ FE/src/pages/index.vue
@@ -1,17 +1,50 @@
-<script setup>
+<script setup></script>
 
-</script>
 <template>
-    <div>
-
-    </div>
+  <div class="post__container">
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+    <HfPost></HfPost>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
+.post__container {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 5%;
+  padding-top: 30px;
 
+  @include mobile {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  @include small-tablet {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
 </style>
+
 <route lang="yaml">
-    name: Home
-    meta:
-      layout: default
+name: Home
+meta:
+  layout: default
 </route>
