e31dae723c3cf30453699a2f07496a948d95da42
Tranggg1210 <nguyenthitrang.ttd@gmail.com>
2024-06-01 17:38:25 +0700

    ✅ complete admin

--- FE/src/api/admin.api.js
+++ FE/src/api/admin.api.js
@@ -2,7 +2,8 @@ import { ApiConstant } from "@/constant/api.constant";
 import { api } from ".";
 
 const adminApi = () => ({
-    checkAdmin: async(id) => api.get(`${ApiConstant.admin.checkAdmin}/${id}`)
+    checkAdmin: async(id) => api.get(`${ApiConstant.admin.checkAdmin}/${id}`),
+    getAnalysisToday: async() => api.get(`${ApiConstant.analysises.getAnalysises}`)
 })
 
-export const { checkAdmin } = adminApi();
+export const { checkAdmin,getAnalysisToday } = adminApi();
